b3d7c1868aa2a0d15fca70a3ec7b5813771fcc41
Rich Bielby <richard.bielby@education.gov.uk>
2023-03-01 14:40:35 +0000

    Cleaned out some unecessary code

--- ui.R
+++ ui.R
@@ -124,7 +124,6 @@ ui <- function(input, output, session) {
       id = "navlistPanel",
       widths = c(2, 8),
       well = FALSE,
-      homepage_panel(),
       dashboard_panel(),
       a11y_panel(),
       support_links()
